3a74461ace998dcb5a89a9ab5105591ba1e6f5b3
Susan Zea <susanzea16@gmail.com>
2021-11-03 16:32:41 -0400

    working on easy methods

--- questions.rb
+++ questions.rb
@@ -31,11 +31,26 @@ class Question
         Question.new(question.first) 
     end
 
+    def self.find_by_author_id(author_id)
+        questions = QuestionsDBConnection.instance.execute(<<-SQL, author_id)
+        SELECT *
+            FROM questions
+            WHERE author_id = ?
+        SQL
+        return nil if questions.empty?
+
+        questions.map { |question| Question.new(question) }
+    end
+
     def initialize(options)
         @id = options['id']
         @title = options['title']
         @body = options['body']
         @associated_author_id = options['associated_author_id']
+    end
+
+    def author
+
     end
 end
 
@@ -76,6 +91,118 @@ class User
         @lname = options['lname']
     end
 
+    def authored_questions
+        Question.find_by_author_id(@id)
+    end
+
+    def authored_replies
+        Reply.find_by_user_id(@id)
+    end
+
 end
 
-class 
+class QuestionFollows
+    attr_accessor :user_id, :question_id, :id
+
+    def self.all
+        data = QuestionsDBConnection.instance.execute("SELECT * FROM question_follows")
+        data.map { |datum| QuestionFollows.new(datum) }
+    end
+
+    def self.find_by_id(id)
+        question_follow = QuestionsDBConnection.instance.execute(<<-SQL, id)
+            SELECT *
+            FROM question_follows
+            WHERE id = ?
+        SQL
+
+        return nil if question_follow.empty?
+
+        QuestionFollows.new(question_follow.first) 
+    end
+
+    def initialize(options)
+        @id = options['id']
+        @user_id = options['user_id']
+        @question_id = options['question_id']
+    end
+end
+
+
+class Reply
+    attr_accessor :id, :question_id, :author_id, :body, :parent_reply_id
+
+    def self.all
+        data = QuestionsDBConnection.instance.execute("SELECT * FROM replies")
+        data.map { |datum| Reply.new(datum) }
+    end
+
+    def self.find_by_id(id)
+        reply = QuestionsDBConnection.instance.execute(<<-SQL, id)
+            SELECT *
+            FROM replies
+            WHERE id = ?
+        SQL
+        return nil if reply.empty?
+
+        Reply.new(reply.first) 
+    end
+
+    def self.find_by_user_id(author_id)
+        replies = QuestionsDBConnection.instance.execute(<<-SQL, author_id)
+            SELECT *
+            FROM replies
+            WHERE author_id = ?
+        SQL
+        return nil if replies.empty?
+
+        replies.map { |reply| Reply.new(reply) }
+    end
+
+    def self.find_by_question_id(question_id)
+        replies = QuestionsDBConnection.instance.execute(<<-SQL, question_id)
+            SELECT *
+            FROM replies
+            WHERE question_id = ?
+        SQL
+        return nil if replies.empty?
+
+        replies.map { |reply| Reply.new(reply) }
+    end
+
+    def initialize(options)
+        @id = options['id']
+        @question_id = options['question_id']
+        @author_id = options['author_id']
+        @parent_reply_id = options['parent_reply_id']
+        @body = options['body']
+    end    
+end
+
+
+class QuestionLike
+    attr_accessor :id, :question_id, :liker_id
+
+    def self.all
+        data = QuestionsDBConnection.instance.execute("SELECT * FROM question_likes")
+        data.map { |datum| QuestionLike.new(datum) }
+    end
+
+    def self.find_by_id(id)
+        question_like = QuestionsDBConnection.instance.execute(<<-SQL, id)
+            SELECT *
+            FROM question_likes
+            WHERE id = ?
+        SQL
+        return nil if question_like.empty?
+
+        QuestionLike.new(question_like.first) 
+    end
+
+    def initialize(options)
+        @id = options['id']
+        @question_id = options['question_id']
+        @liker_id = options['liker_id']
+    end    
+end
+
