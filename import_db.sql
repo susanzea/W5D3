d1b57b6e52f45e68ae05c891b33556ed3f867715
AndyAYu <andyhkgw@gmail.com>
2021-11-03 12:03:50 -0400

    inerting values

--- import_db.sql
+++ import_db.sql
@@ -1,3 +1,4 @@
+PRAGMA foreign_keys = ON; 
 
 CREATE TABLE users(
     id INTEGER PRIMARY KEY,
@@ -17,8 +18,33 @@ CREATE TABLE questions(
 CREATE TABLE question_follows (
     id INTEGER PRIMARY KEY,
     user_id INTEGER NOT NULL,
-    question_id INTEGER NOT NULL
+    question_id INTEGER NOT NULL,
 
     FOREIGN KEY (user_id) REFERENCES users(id)
     FOREIGN KEY (question_id) REFERENCES questions(id)
 );
+
+CREATE TABLE replies (
+    id INTEGER PRIMARY KEY,
+    question_id INTEGER NOT NULL,
+    parent_reply_id INTEGER,
+    author_id INTEGER NOT NULL,
+
+    FOREIGN KEY (question_id) REFERENCES questions(id)
+    FOREIGN KEY (parent_reply_id) REFERENCES replies(id)
+    FOREIGN KEY (author_id) REFERENCES users(id)
+);
+
+CREATE TABLE question_likes (
+    id INTEGER PRIMARY KEY,
+    question_id INTEGER NOT NULL,
+    liker_id INTEGER NOT NULL,
+
+    FOREIGN KEY (question_id) REFERENCES questions(id)
+    FOREIGN KEY (liker_id) REFERENCES users(id)
+);
+
+INSERT INTO
+    users (fname, lname)
+VALUES
+    ("Andy", "Yu")
