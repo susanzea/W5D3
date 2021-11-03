f6f1da753dcde0ff3436027ab1184184c0fa8edd
AndyAYu <andyhkgw@gmail.com>
2021-11-03 15:19:40 -0400

    writing replies class

--- questions.rb
+++ questions.rb
@@ -1,7 +1,7 @@
 require 'sqlite3'
 require 'Singleton'
 
-class QuestionsDBConnection < SQLite3 :: Database
+class QuestionsDBConnection < SQLite3::Database
     include Singleton
 
     def initialize
@@ -11,15 +11,24 @@ class QuestionsDBConnection < SQLite3 :: Database
     end
 end
 
-class Questions
+class Question
+
+    attr_accessor :id, :title, :body, :associated_author_id
 
     def self.all
         data = QuestionsDBConnection.instance.execute("SELECT * FROM questions")
-        data.map { |datum| Questions.new(datum) }
+        data.map { |datum| Question.new(datum) }
     end
 
     def self.find_by_id(id)
-        
+        question = QuestionsDBConnection.instance.execute(<<-SQL, id)
+            SELECT *
+            FROM questions
+            WHERE id = ?
+        SQL
+        return nil if question.empty?
+
+        Question.new(question.first) 
     end
 
     def initialize(options)
@@ -28,8 +37,45 @@ class Questions
         @body = options['body']
         @associated_author_id = options['associated_author_id']
     end
+end
 
+class User
 
+    attr_accessor :id, :fname, :lname
 
+    def self.all
+        data = QuestionsDBConnection.instance.execute("SELECT * FROM users")
+        data.map { |datum| User.new(datum) }
+    end
+
+    def self.find_by_name(fname,lname)
+         user = QuestionsDBConnection.instance.execute(<<-SQL, fname,lname)
+            SELECT *
+            FROM users
+            WHERE fname = ? AND lname = ?
+        SQL
+        return nil if user.empty?
+
+        User.new(user.first) 
+    end
+
+    def self.find_by_id(id)
+        user = QuestionsDBConnection.instance.execute(<<-SQL, id)
+            SELECT *
+            FROM users
+            WHERE id = ?
+        SQL
+        return nil if user.empty?
+
+        User.new(user.first) 
+    end
+
+    def initialize(options)
+        @id = options['id']
+        @fname = options['fname']
+        @lname = options['lname']
+    end
 
 end
+
+class 
